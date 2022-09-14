6a16e008295ba2a68fb40075656219f329982580
Yang-soeun <soeun8636@naver.com>
2022-09-14 14:35:21 +0900

    타임리프-스프링 통합과 폼

--- springmvc4/form/src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ springmvc4/form/src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -42,5 +42,7 @@ public class ItemRepository {
     public void clearStore() {
         store.clear();
     }
-
 }
+
+
+
--- springmvc4/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ springmvc4/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,7 +1,9 @@
 package hello.itemservice.web.form;
 
+import hello.itemservice.domain.item.DeliveryCode;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
+import hello.itemservice.domain.item.ItemType;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
@@ -9,7 +11,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Controller
@@ -18,7 +23,32 @@ import java.util.List;
 public class FormItemController {
 
     private final ItemRepository itemRepository;
+    @ModelAttribute("regions")//어떤 컨트롤러의 메소드를 호출하든 모델에 항상 담기도록 함
+    public Map<String, String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>();
 
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+
+        return regions;
+    }
+
+    @ModelAttribute("itemTypes")
+    public ItemType[] itemTypes() {
+        return ItemType.values();
+    }
+
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes() {
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+
+        deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW", "느린린 배송"));
+
+        return deliveryCodes;
+    }
     @GetMapping
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
@@ -30,12 +60,31 @@ public class FormItemController {
     public String item(@PathVariable long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+
+        //자동으로 담기게 해놨기 때문에 삭제 가능
+//        Map<String, String> regions = new LinkedHashMap<>();//해시맵을 쓰면 순서가 보장이 안되므로
+//
+//        regions.put("SEOUL", "서울");
+//        regions.put("BUSAN", "부산");
+//        regions.put("JEJU", "제주");
+//
+//        model.addAttribute("regions", regions);
+
         return "form/item";
     }
 
     @GetMapping("/add")
     public String addForm(Model model) {
         model.addAttribute("item", new Item());
+
+//        Map<String, String> regions = new LinkedHashMap<>();//해시맵을 쓰면 순서가 보장이 안되므로
+//
+//        regions.put("SEOUL", "서울");
+//        regions.put("BUSAN", "부산");
+//        regions.put("JEJU", "제주");
+//
+//        model.addAttribute("regions", regions);
+
         return "form/addForm";
     }
 
@@ -43,6 +92,8 @@ public class FormItemController {
     public String addItem(@ModelAttribute Item item, RedirectAttributes redirectAttributes) {
 
         log.info("item.open={}", item.getOpen());
+        log.info("item.regions={}", item.getRegions());
+        log.info("item.itemType={}", item.getItemType());
 
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
@@ -54,6 +105,15 @@ public class FormItemController {
     public String editForm(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
+
+//        Map<String, String> regions = new LinkedHashMap<>();//해시맵을 쓰면 순서가 보장이 안되므로
+//
+//        regions.put("SEOUL", "서울");
+//        regions.put("BUSAN", "부산");
+//        regions.put("JEJU", "제주");
+//
+//        model.addAttribute("regions", regions);
+
         return "form/editForm";
     }
 
@@ -62,6 +122,5 @@ public class FormItemController {
         itemRepository.update(itemId, item);
         return "redirect:/form/items/{itemId}";
     }
-
 }
 
