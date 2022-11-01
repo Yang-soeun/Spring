9971c3aebd86db7973a296dccf4fd5792d710ffb
Yang-soeun <soeun8636@naver.com>
2022-11-01 21:51:10 +0900

    연관관계 매핑

--- jpa/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpa/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -11,21 +11,38 @@ public class Order {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @Column(name = "MEMBER_ID")
-    private Long memberId;
+//    @Column(name = "MEMBER_ID")
+//    private Long memberId;
 
+    @ManyToOne
+    @JoinColumn(name = "MEMBER_ID")
     private Member member;
-
+    private LocalDateTime orderDate;
+    @Enumerated(EnumType.STRING) //ORDINAL을 사용하면 순서가 꼬여서 장애가 발생할 수 있으므로 STRING으로 해주기
+    private OrderStatus status;
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
     public Member getMember() {
         return member;
     }
-
     public void setMember(Member member) {
         this.member = member;
     }
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+    public OrderStatus getStatus() {
+        return status;
+    }
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 
-    private LocalDateTime orderDate;
-
-    @Enumerated(EnumType.STRING) //ORDINAL을 사용하면 순서가 꼬여서 장애가 발생할 수 있으므로 STRING으로 해주기
-    private OrderStatus status;
 }
