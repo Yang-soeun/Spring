205c5d2892fd9136cdf7bc3fe74afb067ea9537f
Yang-soeun <soeun8636@naver.com>
2022-12-29 13:40:25 +0900

    웹 계층 개발

--- jpa/jpabook/src/test/java/jpabook/jpabook/JpabookApplicationTests.java
+++ jpa/jpabook/src/test/java/jpabook/jpabook/JpabookApplicationTests.java
@@ -1,7 +1,6 @@
 package jpabook.jpabook;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
 
 @SpringBootTest
 class JpabookApplicationTests {
--- jpa/jpabook/src/test/java/jpabook/jpabook/service/MemberServiceTest.java
+++ jpa/jpabook/src/test/java/jpabook/jpabook/service/MemberServiceTest.java
@@ -2,13 +2,11 @@ package jpabook.jpabook.service;
 
 import jpabook.jpabook.domain.Member;
 import jpabook.jpabook.repository.MemberRepository;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import javax.persistence.EntityManager;
 
