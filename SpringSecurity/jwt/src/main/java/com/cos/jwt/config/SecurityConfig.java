8e9f33a87de05968073f8d14898776673479f804
Yang-soeun <soeun8636@naver.com>
2023-01-28 18:22:19 +0900

    JWT 서버구축

---
+++ SpringSecurity/jwt/src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.cos.jwt.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig{
+
+    private final CorsFilter corsFilter;
+    /*
+    기존: WebSecurityConfigurerAdapter를 상속하고 configure 메소드를 오버라이딩하여 설정하는 방법
+    //https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
+
+    @Override
+    protected void configure(HttpSecurity http) throws  Exception{
+        http.csrf().disable();
+        http.authorizeRequests()
+            .antMatchers("/user/**").authenticated()
+            .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+            .antMatchers("/admin").access("\"hasRole('ROLE_ADMIN')")
+            .anyRequest().permitAll();
+    }
+     */
+
+    //=> 현재: SecurityFilterChain을 리턴하는 메소드를 빈에 등록하는 방식(컴포넌트 방식으로 컨테이너가 관리)
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .addFilter(corsFilter)
+        .formLogin().disable()
+        .httpBasic().disable()
+        .authorizeRequests()
+                .antMatchers("/api/v1/user/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/manager/**")
+                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("/api/v1/admin/**")
+                .access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+
+        return http.build();
+    }
+
+}
